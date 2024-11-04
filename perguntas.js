ab1b269e8255ef10c6e2cfe41757423721b127d5
MariaaPaulla <161321105+MariaaPaulla@users.noreply.github.com>
2024-11-04 08:13:06 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,89 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Matemática',
+    'Como se calcula a área de um triângulo?',
+    'A área de um triângulo qualquer pode ser calculada, multiplicando a base pela sua altura e dividindo esse resultado por 2.'
+
+)
+criaCartao(
+    'Esporte',
+    'Qual é a duração de uma partida de handebol?',
+    'Uma partida de handebol é composta por dois tempos de 30 minutos cada, com um intervalo de 10 minutos entre eles'
+)
+
+criaCartao(
+    'Astronomia',
+    'O que é uma estrela?',
+    'Uma etrela é um corpo celeste que emite luz e calor devido a reações nucleares em seu interior, sendo o Sol o exemplo mais próximo da Terra'
+)
+
+criaCartao(
+    'Química',
+    'O que é um ácido?',
+    'Um ácido é uma substância que, quando dissolvida em água, libera íons de hidrôgenio (H+) e tem um pH inferior a 7'
+)
+
+criaCartao(
+    'Moda',
+    'O que é fast fashion?',
+    'Fast fashion é uma produção rápida e barata de roupas para acompanhar tendências'
+)
+
+criaCartao(
+    'Física',
+    'O que é a lei da gravidade?',
+    'A lei da gravidade diz que corpos se atraem com uma força proporcional ás suas massas'
+)
+
+criaCartao(
+    'Biotecnologia',
+    'O que é biotecnologia?',
+    'É o uso de organismos vivos ou seus componentes para desenvolver produtos e processos úteis'
+)
+
+criaCartao(
+    'Filosofia',
+    'O que é ética?',
+    'É o ramo da filosofia que estuda o príncipios do bem e do mal e como devemos agir '
+)
+
+criaCartao(
+    'Psicologia',
+    'O que é psicanálise?',
+    'É uma abordagem terapêutica que investiga o incosciente para entender e tratar conflitos emocionais '
+)
+
+criaCartao(
+    'Moda',
+    'O que é a marca Dior?',
+    'é uma renomada casa de moda francesa, famosa por suas roupas de alta costura, perfumes e acessórios luxuosos'
+)
+
+criaCartao(
+    'Maquiagem',
+    'Para que serve a base na maquiagem?',
+    'Um produto utilizado para uniformizar o tom da pele e criar uma superfície lisa para outros produtos de maquiagem '
+)
